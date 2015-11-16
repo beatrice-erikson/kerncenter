2a7180b9f2e322ab9826f477123b58413b500937
ktuningj <gotem8@protonmail.ch>
2015-11-16 22:54:23 +0000

    finally the dummy data is generic

--- app/controllers/resource_controller.rb
+++ app/controllers/resource_controller.rb
@@ -4,19 +4,19 @@ class ResourceController < ApplicationController
 	
     	# dummy code below for a line graph
     	hours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    	black_water = [45.0, 34.6, 77.4, 12.0, 55.4, 33.4, 48.8, 64.3, 12.3, 32.1]
-    	grey_water = [90.7, 129.5, 45.4, 44.8, 102.5, 66.7, 93.7, 77.8, 88.9, 99.9]
+    	resource_usage = [45.0, 34.6, 77.4, 12.0, 55.4, 33.4, 48.8, 64.3, 12.3, 32.1]
+    	resource_generation = [90.7, 129.5, 45.4, 44.8, 102.5, 66.7, 93.7, 77.8, 88.9, 99.9]
     	@chart = LazyHighCharts::HighChart.new("graph") do |g|
-      		g.title(text: "Water Usage")
+      		g.title(text: @resource + " Over Time")
       		g.xAxis(categories: hours)
-      		g.series(name: "black", data: black_water)
-      		g.series(name: "grey", data: grey_water)
+      		g.series(name: @resource + " Usage", data: resource_usage)
+      		g.series(name: @resource + " Generation", data: resource_generation)
     	end
     	# dummy code for a bar graph
     	@graph = LazyHighCharts::HighChart.new('column') do |b|
-    		b.series(:name=>'black',:data=> [45.0, 34.6, 77.4, 12.0, 55.4, 33.4, 48.8, 64.3, 12.3, 32.1])
-    		b.series(:name=>'grey',:data=> [90.7, 129.5, 45.4, 44.8, 102.5, 66.7, 93.7, 77.8, 88.9, 99.9])       
-    		b.title({ :text=>"Water Consumption"})
+    		b.series(:name => @resource  + " Usage",:data=> [45.0, 34.6, 77.4, 12.0, 55.4, 33.4, 48.8, 64.3, 12.3, 32.1])
+    		b.series(:name => @resource + " Generation",:data=> [90.7, 129.5, 45.4, 44.8, 102.5, 66.7, 93.7, 77.8, 88.9, 99.9])       
+    		b.title({ :text => @resource + " Over Time"})
     		b.legend({:align => 'left', 
     			:x => -100, 
     			:verticalAlign => 'top',
