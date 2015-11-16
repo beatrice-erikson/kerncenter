be88c12dbaf441722bdc6ef0519bac16001a514c
ktuningj <gotem8@protonmail.ch>
2015-11-16 20:55:07 +0000

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require highcharts/highcharts-more
+//= require highcharts/highcharts
+//= require highcharts/highstock
+//= require highcharts/modules/exporting
+
