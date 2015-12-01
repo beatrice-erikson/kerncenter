a751806030c5d3499ab9eb71437383959959369e
B Corfman <bmc12@hampshire.edu>
2015-11-30 19:28:10 -0500

    Added rough time dropdown select/time routing, added _nav framework

--- app/controllers/resource_controller.rb
+++ app/controllers/resource_controller.rb
@@ -3,8 +3,8 @@ class ResourceController < ApplicationController
 	@resource = params[:resource]
 	@sensors = Type.find_by_resource(@resource).sensors
 	hours = @sensors.first.times
-	resource_usage = @sensors.first.amounts(DateTime.new(2015,11,30), DateTime.new(2015,12,1))
-	resource_generation = @sensors.second.amounts(DateTime.new(2015,11,30), DateTime.new(2015,12,1))
+	resource_usage = @sensors.first.amounts(params[:start], params[:stop])
+	resource_generation = @sensors.second.amounts(params[:start], params[:stop])
 	@resource_vars = [@resource, resource_usage, resource_generation, hours]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'resource/:resource/:start/:stop', to: 'resource#show'
   get 'resource/:resource', to: 'resource#show', as: :resource
 
   root 'dash#index'
