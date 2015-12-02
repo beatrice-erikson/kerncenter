4b486cb604b9a012298aff9c1dd4305322553c68
ktuningj <gotem8@protonmail.ch>
2015-12-02 22:34:52 +0000

    updated parameters with @subtypes

--- app/controllers/resource_controller.rb
+++ app/controllers/resource_controller.rb
@@ -2,9 +2,10 @@ class ResourceController < ApplicationController
   def show
 	@resource = params[:resource]
 	@sensors = Type.find_by_resource(@resource).sensors
-	hours = @sensors.first.times
-	resource_usage = @sensors.first.amounts(params[:start], params[:stop])
-	resource_generation = @sensors.second.amounts(params[:start], params[:stop])
-	@resource_vars = [@resource, resource_usage, resource_generation, hours]
+  @subtypes = Type.find_by_resource(@resource).subtypes
+	@hours = @sensors.first.times
+	@resource_usage = @sensors.first.amounts(params[:start], params[:stop])
+	@resource_generation = @sensors.second.amounts(params[:start], params[:stop])
+	@resource_vars = [@resource, @subtypes, @resource_usage, @resource_generation, @hours]
   end
 end
