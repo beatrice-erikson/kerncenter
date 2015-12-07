f2a658f4c1411cd626a05d8d4c19cfccbe92a75d
Mike Dean <msd14@hampshire.edu>
2015-12-06 22:18:44 -0500

    Modified granularity function
    Granularity function now outputs the beginning and end of each day as a
    separate DateTime

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,14 @@
 module ApplicationHelper
 	require 'date'
-	def granularity(startTime, stopTime)
+  require 'active_support'
+  require 'active_support/core_ext/date_time'
+  def granularity(startTime, stopTime)
 		grain = (stopTime - startTime) / 5
 		times = []
 		while stopTime > startTime do
-			times.push([startTime, startTime + grain - 0.0000000001])
+			times.push([startTime.beginning_of_day(), startTime.end_of_day()])
 			startTime += grain
 		end
 		return times
-	end
+  end
 end
