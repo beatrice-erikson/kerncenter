63a616d66329c49b58927e4f8d5da4aac0133367
jaimejdavila <30884362+jaimejdavila@users.noreply.github.com>
2017-08-20 21:16:48 -0400

    Delete 20151120200223_create_measurements.rb

--- migrate/20151120200223_create_measurements.rb
+++
@@ -1,8 +0,0 @@
-class CreateMeasurements < ActiveRecord::Migration
-  def change
-    create_table :measurements do |t|
-      t.references :sensor, index: true
-    end
-    add_foreign_key :measurements, :sensors
-  end
-end
