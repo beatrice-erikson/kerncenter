8d7c8c45eea3a08452f14cc86023386a96e84e58
jaimejdavila <30884362+jaimejdavila@users.noreply.github.com>
2017-08-20 21:17:21 -0400

    Delete 20151120230330_add_time_and_amount_to_measurements.rb

--- migrate/20151120230330_add_time_and_amount_to_measurements.rb
+++
@@ -1,6 +0,0 @@
-class AddTimeAndAmountToMeasurements < ActiveRecord::Migration
-  def change
-    add_column :measurements, :time, :timestamp
-    add_column :measurements, :amount, :integer
-  end
-end
