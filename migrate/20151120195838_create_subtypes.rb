4cbe71f01d50c7028bb1b54dd75ded8fe696d60f
jaimejdavila <30884362+jaimejdavila@users.noreply.github.com>
2017-08-20 21:16:19 -0400

    Delete 20151120195838_create_subtypes.rb

--- migrate/20151120195838_create_subtypes.rb
+++
@@ -1,9 +0,0 @@
-class CreateSubtypes < ActiveRecord::Migration
-  def change
-    create_table :subtypes do |t|
-      t.references :type, index: true
-      t.string :name
-    end
-    add_foreign_key :subtypes, :types
-  end
-end
